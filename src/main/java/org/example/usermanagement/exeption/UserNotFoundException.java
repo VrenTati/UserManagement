622f375166e022d670fd6761be84922956cae50c
VrenTati <kateryna.vynokurova.pz.2022@lpnu.ua>
2023-12-26 00:46:08 +0200

    Try to dockerize application

---
+++ src/main/java/org/example/usermanagement/exeption/UserNotFoundException.java
@@ -0,0 +1,12 @@
+package org.example.usermanagement.exeption;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
